5c4fd3253d93bc4e920c9597e9b0452d070de832
tangmengjin <tangmengjin@bolod.xin>
2019-10-14 17:14:25 +0800

    新增异常时截屏处理

--- pyTest/appium/desired_capabilities.py
+++ pyTest/appium/desired_capabilities.py
@@ -8,16 +8,20 @@ def get_desired_capabilities():
     #华为荣耀6P
     desired_caps['platformVersion']='4.4.2'
     desired_caps['deviceName'] = 'F8UDU15818002253'
+    desired_caps['normalName'] = u'荣耀6P'
     '''
     #华为荣耀X1
     desired_caps['platformVersion']='4.4'
     desired_caps['deviceName'] = 'T7K6R14B01000626'
-    #小米NOTE6A
+    desired_caps['normalName'] = u'荣耀X1'
+    #红米NOTE6A
     desired_caps['platformVersion']='9'
     desired_caps['deviceName'] = '0d68e2427d32'
+    desired_caps['normalName'] = u'红米NOTE6A'
     #华为荣耀畅玩7
     desired_caps['platformVersion']='8.1.0'
     desired_caps['deviceName']='HFK9K19330910557'
+    desired_caps['normalName'] = u'荣耀畅玩7'
     '''
 
     desired_caps['noReset']=True
--- pyTest/appium/main.py
+++ pyTest/appium/main.py
@@ -5,8 +5,25 @@ import desired_capabilities
 from unittest import TestCase
 import unittest
 import time
-
+import sys
+import os
+sys.path.append("..")
+import tmjLog as lxLog
 driver = None
+
+def saveScreen(driver, e):
+    baseImagePath = 'D:/CODE/pyTest/pyTest/exception/'
+    desired_caps = desired_capabilities.get_desired_capabilities()
+    phone = desired_caps['normalName']
+    currentTime = time.strftime("%Y%m%d%H%M%S", time.localtime())
+    saveFile = baseImagePath + phone
+    mkdirlambda = lambda x: os.makedirs(x) if not os.path.exists(x)  else True
+    mkdirlambda(saveFile)
+    saveFile = saveFile + '/' + currentTime + '.png'
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    lxLog.getDebugLog()(u"saveScreen\n截图：%s\n错误文件:%s:%d\n错误原因:%s", \
+        saveFile, exc_tb.tb_frame.f_code.co_filename, exc_tb.tb_lineno, str(e))
+    driver.get_screenshot_as_file(saveFile)
 
 def setUp():
     # 获取我们设定的capabilities，通知Appium Server创建相应的会话。
@@ -19,10 +36,10 @@ def setUp():
     if None == driver:
         return
     else:
-        print 'setUp ok'
+        lxLog.getDebugLog()('setUp ok')
 
 def test_getPermit():
-    print 'test_getPermit begin'
+    lxLog.getDebugLog()('test_getPermit begin')
     while True:
         time.sleep(1)
         try:
@@ -37,27 +54,124 @@ def test_getPermit():
                 break
         except Exception as e:
             print e
+            saveScreen(driver, e)
             break
-    print 'test_getPermit over'
+    lxLog.getDebugLog()('test_getPermit over')
    
 def test_audioDial():  
-    print 'test_audioDial begin'      
-    time.sleep(3)
+    lxLog.getDebugLog()('test_audioDial begin')
+    try:
+        btnAudioDial = driver.find_element_by_id('com.lx.netphone:id/btn_dial')
+        btnAudioDial.click()
+        time.sleep(3)
+        btnAudioMute = driver.find_element_by_id('com.lx.netphone:id/button_audio_mute')
+        btnAudioMute.click()
+        time.sleep(3)
+        btnAudioSpeaker = driver.find_element_by_id('com.lx.netphone:id/button_audio_speaker')
+        btnAudioSpeaker.click()
+        time.sleep(3)
+        btnAudioHangup = driver.find_element_by_id('com.lx.netphone:id/button_audio_hang_up')
+        btnAudioHangup.click()
+        time.sleep(3)
+    except Exception as e:
+        print e
+        saveScreen(driver, e)
    
 def test_audioAccept(): 
-    print 'test_audioAccept begin'           
-    time.sleep(3)
+    lxLog.getDebugLog()('test_audioAccept begin')
+    try:
+        btnAudioDial = driver.find_element_by_id('com.lx.netphone:id/btn_answer')
+        btnAudioDial.click()
+        time.sleep(3)
+        btnAudioMute = driver.find_element_by_id('com.lx.netphone:id/button_audio_mute')
+        btnAudioMute.click()
+        time.sleep(3)
+        btnAudioSpeaker = driver.find_element_by_id('com.lx.netphone:id/button_audio_speaker')
+        btnAudioSpeaker.click()
+        time.sleep(3)
+        btnAudioHangup = driver.find_element_by_id('com.lx.netphone:id/button_audio_hang_up')
+        btnAudioHangup.click()
+        time.sleep(3)
+    except Exception as e:
+        print e
+        saveScreen(driver, e)
    
-def test_videoDial():  
-    print 'test_videoDial begin'          
-    time.sleep(3)
+def test_videoDial1():  
+    lxLog.getDebugLog()('test_videoDial1 begin')
+    try:
+        btnVideoDial = driver.find_element_by_id('com.lx.netphone:id/btn_videoDial')
+        btnVideoDial.click()
+        time.sleep(3)
+        btnVideoSwitchCamera = driver.find_element_by_id('com.lx.netphone:id/video_image_change_camera')
+        btnVideoSwitchCamera.click()
+        time.sleep(3)
+        btnVideoSwitchToAudio = driver.find_element_by_id('com.lx.netphone:id/video_image_change_voice_call')
+        btnVideoSwitchToAudio.click()
+        time.sleep(3)
+        btnAudioHangup = driver.find_element_by_id('com.lx.netphone:id/button_audio_hang_up')
+        btnAudioHangup.click()
+        time.sleep(3)
+    except Exception as e:
+        print e
+        saveScreen(driver, e)
    
-def test_videoAccept():  
-    print 'test_videoAccept begin'          
-    time.sleep(3)
+def test_videoDial2():  
+    lxLog.getDebugLog()('test_videoDial2 begin')
+    try:
+        btnVideoDial = driver.find_element_by_id('com.lx.netphone:id/btn_videoDial')
+        btnVideoDial.click()
+        time.sleep(3)
+        btnVideoSwitchCamera = driver.find_element_by_id('com.lx.netphone:id/video_image_change_camera')
+        btnVideoSwitchCamera.click()
+        time.sleep(3)
+        btnVideoSwitchCamera.click()
+        time.sleep(3)
+        btnVideoHangup = driver.find_element_by_id('com.lx.netphone:id/video_image_hang_up')
+        btnVideoHangup.click()
+        time.sleep(3)
+    except Exception as e:
+        print e
+        saveScreen(driver, e)
+   
+def test_videoAccept1():  
+    lxLog.getDebugLog()('test_videoAccept1 begin')
+    try:
+        btnVideoDial = driver.find_element_by_id('com.lx.netphone:id/btn_videoAnswer')
+        btnVideoDial.click()
+        time.sleep(3)
+        btnVideoSwitchCamera = driver.find_element_by_id('com.lx.netphone:id/video_image_change_camera')
+        btnVideoSwitchCamera.click()
+        time.sleep(3)
+        btnVideoSwitchToAudio = driver.find_element_by_id('com.lx.netphone:id/video_image_change_voice_call')
+        btnVideoSwitchToAudio.click()
+        time.sleep(3)
+        btnAudioHangup = driver.find_element_by_id('com.lx.netphone:id/button_audio_hang_up')
+        btnAudioHangup.click()
+        time.sleep(3)
+    except Exception as e:
+        print e
+        saveScreen(driver, e)
+   
+def test_videoAccept2():  
+    lxLog.getDebugLog()('test_videoAccept2 begin')
+    try:
+        btnVideoDial = driver.find_element_by_id('com.lx.netphone:id/btn_videoAnswer')
+        btnVideoDial.click()
+        time.sleep(3)
+        btnVideoSwitchCamera = driver.find_element_by_id('com.lx.netphone:id/video_image_change_camera')
+        btnVideoSwitchCamera.click()
+        time.sleep(3)
+        btnVideoSwitchCamera.click()
+        time.sleep(3)
+        btnVideoHangup = driver.find_element_by_id('com.lx.netphone:id/video_image_hang_up')
+        btnVideoHangup.click()
+        time.sleep(3)
+    except Exception as e:
+        print e
+        saveScreen(driver, e)
 
 def tearDown(): 
-    print 'tearDown'    
+    lxLog.getDebugLog()('tearDown')
     # 测试结束，退出会话。 
     driver.quit()
 
@@ -66,7 +180,9 @@ if __name__ == '__main__':
     test_getPermit()
     test_audioDial()
     test_audioAccept()
-    test_videoDial()
-    test_videoAccept()
+    test_videoDial1()
+    test_videoAccept1()
+    test_videoDial2()
+    test_videoAccept2()
     tearDown()
 
